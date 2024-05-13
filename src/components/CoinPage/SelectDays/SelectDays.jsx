7d76dd90fcf1e9b694849edc851012df289b86c9
Sameer <sameersattar1111@gmail.com>
2024-05-13 19:10:39 +0500

    components and  pages

---
+++ src/components/CoinPage/SelectDays/SelectDays.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SelectDays = () => {
+  return (
+    <div>SelectDays</div>
+  )
+}
+
+export default SelectDays
---
+++ src/components/ComparePage/SelectCoins/SelectCoins.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SelectCoins = () => {
+  return (
+    <div>SelectCoins</div>
+  )
+}
+
+export default SelectCoins
