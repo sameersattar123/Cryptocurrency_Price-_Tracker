348be30515bfefa8c7e596d2d057687f6dbede51
Sameer <sameersattar1111@gmail.com>
2024-06-02 15:57:40 +0500

    complete

--- src/components/Dashboad/Grid/Grid.jsx
+++ src/components/Dashboad/Grid/Grid.jsx
@@ -2,53 +2,56 @@ import React from "react";
 import "./style.css";
 import TrendingUpIcon from "@mui/icons-material/TrendingUp";
 import TrendingDownIcon from "@mui/icons-material/TrendingDown";
+import { Link } from "react-router-dom";
 
 const Grid = ({ coin }) => {
   return (
-    <div className="grid-container">
-      <div className="info-flex">
-        <img src={coin.image} alt="image" className="coin-image" />
-        <div className="name-col">
-          <p className="coin-symbol">{coin.symbol}</p>
-          <p className="coin-name">{coin.name}</p>
+    <Link to={`/coin/${coin.id}`}>
+      <div className="grid-container">
+        <div className="info-flex">
+          <img src={coin.image} alt="image" className="coin-image" />
+          <div className="name-col">
+            <p className="coin-symbol">{coin.symbol}</p>
+            <p className="coin-name">{coin.name}</p>
+          </div>
+        </div>
+        <div className="chip-flex">
+          <div
+            className={
+              coin.price_change_percentage_24h > 0
+                ? "coin-price-green"
+                : "coin-price-red"
+            }
+          >
+            {coin.price_change_percentage_24h.toFixed(3)}%
+          </div>
+          {coin.price_change_percentage_24h > 0 ? (
+            <div className="icon-chip-green">
+              <TrendingUpIcon />
+            </div>
+          ) : (
+            <div className="icon-chip-red">
+              <TrendingDownIcon />
+            </div>
+          )}
+        </div>
+        <div className="info-container">
+          <h3
+            className={
+              coin.price_change_percentage_24h > 0 ? "price-green" : "price-red"
+            }
+          >
+            ${coin.current_price.toLocaleString()}
+          </h3>
+          <p className="total-volume">
+            Total Volume : {coin.total_volume.toLocaleString()}
+          </p>
+          <p className="market-cap">
+            Market Cap : {coin.market_cap.toLocaleString()}
+          </p>
         </div>
       </div>
-      <div className="chip-flex">
-        <div
-          className={
-            coin.price_change_percentage_24h > 0
-              ? "coin-price-green"
-              : "coin-price-red"
-          }
-        >
-          {coin.price_change_percentage_24h.toFixed(3)}%
-        </div>
-        {coin.price_change_percentage_24h > 0 ? (
-          <div className="icon-chip-green">
-            <TrendingUpIcon />
-          </div>
-        ) : (
-          <div className="icon-chip-red">
-            <TrendingDownIcon />
-          </div>
-        )}
-      </div>
-      <div className="info-container">
-        <h3
-          className={
-            coin.price_change_percentage_24h > 0 ? "price-green" : "price-red"
-          }
-        >
-          ${coin.current_price.toLocaleString()}
-        </h3>
-        <p className="total-volume">
-          Total Volume : {coin.total_volume.toLocaleString()}
-        </p>
-        <p className="market-cap">
-          Market Cap : {coin.market_cap.toLocaleString()}
-        </p>
-      </div>
-    </div>
+    </Link>
   );
 };
 
--- src/components/Dashboad/List/List.jsx
+++ src/components/Dashboad/List/List.jsx
@@ -2,61 +2,64 @@ import React from "react";
 import "./style.css";
 import TrendingUpIcon from "@mui/icons-material/TrendingUp";
 import TrendingDownIcon from "@mui/icons-material/TrendingDown";
+import { Link } from "react-router-dom";
 
 const List = ({ coin }) => {
   return (
-    <tr className="list-row">
-      <td className="info-flex">
-        <img src={coin.image} alt="image" className="coin-image" />
-      </td>
-      <td>
-        <div className="name-col">
-          <p className="coin-symbol">{coin.symbol}</p>
-          <p className="coin-name">{coin.name}</p>
-        </div>
-      </td>
-      <td className="chip-flex">
-        <div
-          className={
-            coin.price_change_percentage_24h > 0
-              ? "coin-price-green"
-              : "coin-price-red"
-          }
-        >
-          {coin.price_change_percentage_24h.toFixed(3)}%
-        </div>
+    <Link to={`/coin/${coin.id}`}>
+      <tr className="list-row">
+        <td className="info-flex">
+          <img src={coin.image} alt="image" className="coin-image" />
         </td>
         <td>
-        {coin.price_change_percentage_24h > 0 ? (
-          <div className="icon-chip-green td-icon">
-            <TrendingUpIcon />
+          <div className="name-col">
+            <p className="coin-symbol">{coin.symbol}</p>
+            <p className="coin-name">{coin.name}</p>
           </div>
-        ) : (
-          <div className="icon-chip-red td-icon">
-            <TrendingDownIcon />
+        </td>
+        <td className="chip-flex">
+          <div
+            className={
+              coin.price_change_percentage_24h > 0
+                ? "coin-price-green"
+                : "coin-price-red"
+            }
+          >
+            {coin.price_change_percentage_24h.toFixed(3)}%
           </div>
-        )}
-      </td>
-      <td className="info-container">
-        <h3
-          className={
-            coin.price_change_percentage_24h > 0 ? "price-green" : "price-red"
-          }
-        >
-          ${coin.current_price.toLocaleString()}
-        </h3>
         </td>
         <td>
-        <p className="total-volume td-total-volume">
-        {coin.total_volume.toLocaleString()}
-        </p>
+          {coin.price_change_percentage_24h > 0 ? (
+            <div className="icon-chip-green td-icon">
+              <TrendingUpIcon />
+            </div>
+          ) : (
+            <div className="icon-chip-red td-icon">
+              <TrendingDownIcon />
+            </div>
+          )}
+        </td>
+        <td className="info-container">
+          <h3
+            className={
+              coin.price_change_percentage_24h > 0 ? "price-green" : "price-red"
+            }
+          >
+            ${coin.current_price.toLocaleString()}
+          </h3>
         </td>
         <td>
-        <p className="market-cap td-market-cap">
-        {coin.market_cap.toLocaleString()}
-        </p>
-      </td>
-    </tr>
+          <p className="total-volume td-total-volume">
+            {coin.total_volume.toLocaleString()}
+          </p>
+        </td>
+        <td>
+          <p className="market-cap td-market-cap">
+            {coin.market_cap.toLocaleString()}
+          </p>
+        </td>
+      </tr>
+    </Link>
   );
 };
 
